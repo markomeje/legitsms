bf1a91871425fde01446e59682ebfdcd12a7793c
markomeje <markomejeonline@gmail.com>
2022-07-25 13:12:14 +0100

    Add All

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Pagination\{Paginator, LengthAwarePaginator};
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (config('app.env') === 'production') {
+            $this->app['request']->server->set('HTTPS','on');
+            URL::forceScheme('https');
+        }
+
+        Schema::defaultStringLength(191);
+        Paginator::useBootstrap();
     }
 }
