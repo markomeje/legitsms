b64a779641253ad5edd918d7d90b7e2fd8ad775a
markomeje <markomejeonline@gmail.com>
2022-09-03 18:36:26 +0100

    Increase font size

--- resources/views/frontend/contact/index.blade.php
+++ resources/views/frontend/contact/index.blade.php
@@ -1,7 +1,7 @@
 <div class="frontend-wrapper min-vh-100">
   @include('layouts.header')
   @include('frontend.layouts.navbar')
-    <section class="mt-5 pt-5">
+    <section class="mt-5 py-5">
       <div class="container mt-5">
         <h3 class="text-white mb-4">Contact Request</h3>
           <div class="row">
